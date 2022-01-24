541fa9d208c1b6f484dd05b56ace7dce1bae3a7c
Will Apolinario <willieapolinario@gmail.com>
2022-01-24 20:58:51 -0300

    Component GameItem

--- src/components/CartList/stories.tsx
+++ src/components/CartList/stories.tsx
@@ -1,9 +1,24 @@
 import { Story, Meta } from '@storybook/react'
-import CartList from '.'
+import CartList, { CartListProps } from '.'
+
+import CartListMock from './mock'
 
 export default {
   title: 'CartList',
-  component: CartList
+  component: CartList,
+  args: {
+    items: CartListMock,
+    total: 'R$ 200,00'
+  },
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
 } as Meta
 
-export const Default: Story = () => <CartList />
+export const Default: Story<CartListProps> = (args) => (
+  <div style={{ maxWidth: 800 }}>
+    <CartList {...args} />
+  </div>
+)
--- src/components/CartList/test.tsx
+++ src/components/CartList/test.tsx
@@ -1,14 +1,18 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helper'
 
 import CartList from '.'
+import CartListMock from './mock'
 
 describe('<CartList />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<CartList />)
+  it('should render correctly', () => {
+    const { container } = renderWithTheme(
+      <CartList items={CartListMock} total="R$ 200,00" />
+    )
 
-    expect(
-      screen.getByRole('heading', { name: /CartList/i })
-    ).toBeInTheDocument()
+    expect(screen.getAllByRole('heading')).toHaveLength(2)
+
+    expect(screen.getByText('R$ 200,00')).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
   })
